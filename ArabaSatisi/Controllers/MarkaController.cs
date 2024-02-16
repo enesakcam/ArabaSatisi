e2cd94ece005b06e548f26ee981c39bdcf6471e1
enes482 <enesakcm9@gmail.com>
2024-02-16 09:35:08 +0300

    Proje dosyası ekle.

---
+++ ArabaSatisi/Controllers/MarkaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArabaSatisi.Controllers
+{
+    public class MarkaController : Controller
+    {
+        public JsonResult Index()
+        {
+            return Json(new { 
+            
+            Id=1,
+            Marka="Togg",
+            Model="T10X"
+            });
+        }
+    }
+}
